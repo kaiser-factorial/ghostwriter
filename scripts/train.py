75e9f983e85c884a1deaace6c2482a8953bb7fd2
Corina Kaiser <crk9967@nyu.edu>
2026-06-10 06:20:44 -0700

    chore: increase num_train_epochs from 3 to 5 in training configuration

--- scripts/train.py
+++ scripts/train.py
@@ -43,7 +43,7 @@ DEFAULTS = dict(
     max_seq_len=2048,
     # optimization
     learning_rate=2e-5,
-    num_train_epochs=3,
+    num_train_epochs=5,
     per_device_train_batch_size=4,
     gradient_accumulation_steps=4,
     warmup_ratio=0.03,
